2055210ef65914a809925d2adb302bfab4c99d6e
bionix404 <alltechtamilblogger@gmail.com>
2026-05-25 10:10:16 +0530

    up

--- sequelize.config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * Sequelize CLI configuration wrapper.
- * Unwraps the TypeScript default-exported database configuration object for CLI consumption.
- */
-const databaseModule = require('./dist/config/database');
-
-module.exports = databaseModule.default || databaseModule;
